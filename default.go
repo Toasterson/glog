c9f72efbd3436ab02edd1a089d8a61e86450f957
Till Wegmüller <toasterson@gmail.com>
2017-09-04 19:55:20 +0200

    Initial commit

---
+++ default.go
@@ -0,0 +1,76 @@
+package glog
+
+import (
+	"os"
+	"log"
+)
+
+var stdLog = New(LOG_INFO, os.Stderr, "", log.LstdFlags)
+
+func SetLevelFromString(level string) error{
+	return stdLog.SetLevelFromString(level)
+}
+
+func SetLevel(level int) {
+	stdLog.SetLevel(level)
+}
+
+func SetFlags(flags int) {
+	stdLog.SetFlags(flags)
+}
+
+func Tracef(format string, v ...interface{}) {
+	stdLog.Tracef(format, v)
+}
+
+func Traceln(v ...interface{}) {
+	stdLog.Traceln(v)
+}
+
+func Debugf(format string, v ...interface{}) {
+	stdLog.Debugf(format, v)
+}
+
+func Debugln(v ...interface{}) {
+	stdLog.Debugln(v)
+}
+
+func Infof(format string, v ...interface{}) {
+	stdLog.Infof(format, v)
+}
+
+func Infoln(v ...interface{}) {
+	stdLog.Infoln(v)
+}
+
+func Warnf(format string, v ...interface{}) {
+	stdLog.Warnf(format, v)
+}
+
+func Warnln(v ...interface{}) {
+	stdLog.Warnln(v)
+}
+
+func Errf(format string, v ...interface{}) {
+	stdLog.Errf(format, v)
+}
+
+func Errln(v ...interface{}) {
+	stdLog.Errln(v)
+}
+
+func Critf(format string, v ...interface{}) {
+	stdLog.Critf(format, v)
+}
+
+func Critln(v ...interface{}) {
+	stdLog.Critln(v)
+}
+
+func Emergf(format string, v ...interface{}) {
+	stdLog.Emergf(format, v)
+}
+
+func Emergln(v ...interface{}) {
+	stdLog.Emergln(v)
+}
---
+++ glog.go
@@ -0,0 +1,157 @@
+package glog
+
+import (
+	"log"
+	"io"
+	"strings"
+	"fmt"
+)
+
+const (
+	//To Debug the Application gives a lot of information try grep
+	LOG_TRACE = iota
+	//To Analyse the application
+	LOG_DEBUG = iota
+	//Let the use know something
+	LOG_INFO = iota
+	//Warn the user he may have a problem
+	LOG_WARN = iota
+	//There was an error need to stop routine
+	LOG_ERR = iota
+	//Some Client has not gotten Data
+	LOG_CRIT = iota
+	//Application Panic that quits Even a Server
+	LOG_EMERG = iota
+)
+
+type Logger struct {
+	Level int
+	log   *log.Logger
+}
+
+func New(level int, output io.Writer, prefix string, flags int) *Logger {
+	return &Logger{
+		Level: level,
+		log:   log.New(output, prefix, flags),
+	}
+}
+
+func (l *Logger) SetLevelFromString(level string) error {
+	if !strings.HasPrefix(level, "LOG_") {
+		level = "LOG_" + level
+	}
+	level = strings.ToUpper(level)
+	switch level {
+	case "LOG_TRACE":
+		l.SetLevel(LOG_TRACE)
+		l.SetFlags(log.Llongfile | log.Ltime)
+	case "LOG_DEBUG":
+		l.SetLevel(LOG_DEBUG)
+		l.SetFlags(log.Lshortfile | log.Ltime)
+	case "LOG_INFO":
+		l.SetLevel(LOG_INFO)
+	case "LOG_WARN":
+		l.SetLevel(LOG_WARN)
+	case "LOG_ERR":
+		l.SetLevel(LOG_ERR)
+	case "LOG_CRIT":
+		l.SetLevel(LOG_CRIT)
+	case "LOG_EMERG":
+		l.SetLevel(LOG_EMERG)
+	default:
+		fmt.Errorf("Cannot set Level to %s use trace|debug|info|warn|err|crit|emerg", level)
+	}
+	return nil
+}
+
+func (l *Logger) SetLevel(level int) {
+	l.Level = level
+}
+
+func (l *Logger) SetFlags(flags int) {
+	l.log.SetFlags(flags)
+}
+
+func (l *Logger) Tracef(format string, v ...interface{}) {
+	if l.Level == LOG_TRACE {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Traceln(v ...interface{}) {
+	if l.Level == LOG_TRACE {
+		l.log.Println(v)
+	}
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	if l.Level <= LOG_DEBUG {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Debugln(v ...interface{}) {
+	if l.Level <= LOG_DEBUG {
+		l.log.Println(v)
+	}
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	if l.Level <= LOG_INFO {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Infoln(v ...interface{}) {
+	if l.Level <= LOG_INFO {
+		l.log.Println(v)
+	}
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	if l.Level <= LOG_WARN {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Warnln(v ...interface{}) {
+	if l.Level <= LOG_WARN {
+		l.log.Println(v)
+	}
+}
+
+func (l *Logger) Errf(format string, v ...interface{}) {
+	if l.Level <= LOG_ERR {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Errln(v ...interface{}) {
+	if l.Level <= LOG_ERR {
+		l.log.Println(v)
+	}
+}
+
+func (l *Logger) Emergf(format string, v ...interface{}) {
+	if l.Level <= LOG_EMERG {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Emergln(v ...interface{}) {
+	if l.Level <= LOG_EMERG {
+		l.log.Println(v)
+	}
+}
+
+func (l *Logger) Critf(format string, v ...interface{}) {
+	if l.Level <= LOG_CRIT {
+		l.log.Printf(format, v)
+	}
+}
+
+func (l *Logger) Critln(v ...interface{}) {
+	if l.Level <= LOG_CRIT {
+		l.log.Println(v)
+	}
+}
